d089e03c23e76ad2b763e810906efd2a67c794ac
Antonio-Jefferson <antjeffersonbatista@gmail.com>
2023-01-10 16:11:46 -0300

    Dependencias instaldas e conecção com o mongodb feita co sucesso

--- src/server.js
+++ src/server.js
@@ -1,12 +1,20 @@
 import express from "express";
 import cors from "cors";
 import dotenv from  "dotenv";
+import { MongoClient }  from "mongodb";
+import joi from "joi";
+import dayjs from "dayjs";
 
 dotenv.config();
+const server = express();
 server.use(cors())
 server.use(express.json())
 
-const server = express();
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
 
+let db;
+mongoClient.connect().then(()=> {
+    db = mongoClient.db("bankbatepapouol")
+})
 
-server.listen(process.env.PORT_SEV, ()=> console.log(`Running on the door ${process.env.PORT_SERV}`))
+server.listen(process.env.PORT_SERV, ()=> console.log(`Running on the door ${process.env.PORT_SERV}`))
