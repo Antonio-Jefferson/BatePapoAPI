42b4e8624aa0ba3a321e47038f8ab497dbcbc4f4
Antonio-Jefferson <antjeffersonbatista@gmail.com>
2023-01-11 17:34:10 -0300

    roda de cadastro de usuarios concluida com validações usando a lib joi

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 import express from "express";
 import cors from "cors";
-import dotenv from  "dotenv";
-import { MongoClient }  from "mongodb";
+import dotenv from "dotenv";
+import { MongoClient } from "mongodb";
 import joi from "joi";
-import dayjs from "dayjs";
+import day from "dayjs";
 
 dotenv.config();
 const server = express();
@@ -13,8 +13,49 @@ server.use(express.json())
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
 
 let db;
-mongoClient.connect().then(()=> {
+mongoClient.connect().then(() => {
     db = mongoClient.db("bankbatepapouol")
 })
 
-server.listen(process.env.PORT_SERV, ()=> console.log(`Running on the door ${process.env.PORT_SERV}`))
+const validationName = joi.object({
+    name: joi.string().required().min(3)
+})
+
+server.post("/participants", async (req, res) => {
+    const { name } = req.body;
+    const validation = validationName.validate(req.body);
+
+    if (validation.error) {
+        const error = validation.error.details[0].message;
+        res.status(422).send(error);
+        return;
+    }
+
+    try {
+        const isValid = await db.collection("participants").findOne({ name: name });
+        if (isValid) {
+            res.status(409).send("Usuario já existente!")
+        }
+        const user =
+        {
+            name: name,
+            lastStatus: Date.now()
+        }
+        await db.collection("participants").insertOne(user);
+
+        const message = {
+            from: "xxx",
+            to: "Todos",
+            text: "entra na sala...",
+            type: "status",
+            time: "HH:MM:SS"
+        }
+        await db.collection("messages").insertOne(message)
+        
+        res.sendStatus(201)
+    } catch (error) {
+        res.sendStatus(400);
+    }
+})
+
+server.listen(process.env.PORT_SERV, () => console.log(`Running on the door ${process.env.PORT_SERV}`))
