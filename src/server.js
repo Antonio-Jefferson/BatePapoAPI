31c9c13e6e3a9f8270f6d17190ef2cfbb22d6cb3
Antonio-Jefferson <antjeffersonbatista@gmail.com>
2023-01-12 10:43:25 -0300

    emplementação da rota POST/messages

--- src/server.js
+++ src/server.js
@@ -21,6 +21,14 @@ const validationName = joi.object({
     name: joi.string().required().min(3)
 })
 
+const validationMessages = joi.object({
+    to: joi.string().required(),
+    text: joi.string().required().min(1),
+    type: joi.string().valid("message", "private_message").required(),
+    from: joi.string(),
+    time: joi.string()
+})
+
 server.post("/participants", async (req, res) => {
     const { name } = req.body;
     const validation = validationName.validate(req.body);
@@ -32,9 +40,10 @@ server.post("/participants", async (req, res) => {
     }
 
     try {
-        const isValid = await db.collection("participants").findOne({ name: name });
+        const isValid = await db.collection("participants").findOne({ name });
         if (isValid) {
             res.status(409).send("Usuario já existente!")
+            return;
         }
         const user =
         {
@@ -57,5 +66,48 @@ server.post("/participants", async (req, res) => {
         res.sendStatus(400);
     }
 })
+
+server.get("/participants", async (req, res) => {
+    try {
+        const promise = await db.collection("participants").find().toArray();
+        res.status(200).send(promise);
+    } catch (error) {
+        res.sendStatus(400)
+    }
+})
+
+server.post("/messages", async (req, res)=>{
+    const {to, text, type} = req.body;
+    const {user} = req.headers;
+    try {
+        const messageUser = {
+            to,
+            text,
+            type,
+            from:user,
+            time: day().format("hh:mm:ss")
+        }
+        const validateMessage = validationMessages.validate(messageUser);
+        if(validateMessage.error){
+            const errors = validateMessage.error.details.map(item => item.message);
+            res.status(422).send(errors);
+            return;
+        }
+        const isParticipant = await db.collection("participants").findOne({ name: user });
+        if(!isParticipant){
+            res.status(422).send("Usuario não existe");
+            return;
+        }
+        await db.collection("messages").insertOne(messageUser);
+        res.sendStatus(201)
+    } catch (error) {
+        res.sendStatus(500)
+        console.log(error)
+    }
+})
+server.get("/messages", async (req, res)=>{
+    
+})
+
 
 server.listen(process.env.PORT_SERV, () => console.log(`Running on the door ${process.env.PORT_SERV}`))
