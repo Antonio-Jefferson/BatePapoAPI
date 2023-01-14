43071316e39adab11e28484110a340e1a7358dfd
Antonio-Jefferson <antjeffersonbatista@gmail.com>
2023-01-14 17:27:49 -0300

    todas as emplementações foram feitar sem o bonus

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ const mongoClient = new MongoClient(process.env.DATABASE_URL);
 
 let db;
 mongoClient.connect().then(() => {
-    db = mongoClient.db("bankbatepapouol")
+    db = mongoClient.db()
 })
 
 const validationName = joi.object({
@@ -53,14 +53,14 @@ server.post("/participants", async (req, res) => {
         await db.collection("participants").insertOne(user);
 
         const message = {
-            from: "xxx",
+            from: user,
             to: "Todos",
             text: "entra na sala...",
             type: "status",
-            time: "HH:MM:SS"
+            time: day().format("HH:MM:SS")
         }
         await db.collection("messages").insertOne(message)
-        
+
         res.sendStatus(201)
     } catch (error) {
         res.sendStatus(400);
@@ -76,25 +76,25 @@ server.get("/participants", async (req, res) => {
     }
 })
 
-server.post("/messages", async (req, res)=>{
-    const {to, text, type} = req.body;
-    const {user} = req.headers;
+server.post("/messages", async (req, res) => {
+    const { to, text, type } = req.body;
+    const { user } = req.headers;
     try {
         const messageUser = {
             to,
             text,
             type,
-            from:user,
-            time: day().format("hh:mm:ss")
+            from: user,
+            time: day().format("HH:MM:SS")
         }
         const validateMessage = validationMessages.validate(messageUser);
-        if(validateMessage.error){
+        if (validateMessage.error) {
             const errors = validateMessage.error.details.map(item => item.message);
             res.status(422).send(errors);
             return;
         }
         const isParticipant = await db.collection("participants").findOne({ name: user });
-        if(!isParticipant){
+        if (!isParticipant) {
             res.status(422).send("Usuario não existe");
             return;
         }
@@ -105,9 +105,63 @@ server.post("/messages", async (req, res)=>{
         console.log(error)
     }
 })
-server.get("/messages", async (req, res)=>{
-    
+server.get("/messages", async (req, res) => {
+    const { user } = req.headers;
+    const limit = Number(req.query);
+    try {
+        if (limit && limit > 0) {
+            const data = await db.collection("messages").find().toArray();
+            const messagesFromUser = data.filter((intem) => intem.from === user || intem.to === user || intem.to === "Todos" || intem.type === "message")
+            res.status(200).send(messagesFromUser.slice(-limit))
+        } else {
+            const messages = await db.collection("messages").find().toArray();
+            const messagesFromUser = messages.filter((intem) => intem.from === user || intem.to === "Todos" || intem.type === "message")
+            res.status(200).send(messagesFromUser)
+        }
+    } catch (error) {
+        res.sendStatus(500)
+    }
 })
 
+server.post("/status", async (req, res) => {
+    const { user } = req.headers;
+    try {
+        const isParticipant = await db.collection("participants").find({ name: user });
+        if (!isParticipant) {
+            res.sendStatus(404)
+            return;
+        }
+        await db.collection("participants").updateOne({ name: user }, { $set: { lastStatus: Date.now() } });
+        res.sendStatus(200);
+    } catch (error) {
+        res.sendStatus(500)
+    }
+})
 
-server.listen(process.env.PORT_SERV, () => console.log(`Running on the door ${process.env.PORT_SERV}`))
+setInterval(async () => {
+    const seconds = Date.now() - 10 * 1000; 
+    
+    try {
+      const inactiveUsers = await db
+        .collection("participants").find({ lastStatus: { $lte: seconds } }).toArray();
+  
+      if (inactiveUsers.length > 0) {
+        const inativeMessages = inactiveUsers.map((inactive) => {
+            return {
+              from: inactive.name,
+              to: "Todos",
+              text: "sai da sala...",
+              type: "status",
+              time: day().format("HH:MM:SS"),
+            };
+          }
+        );
+        await db.collection("messages").insertMany(inativeMessages);
+        await db.collection("participants").deleteMany({ lastStatus: { $lte: seconds } });
+      }
+    } catch (error) {
+      res.status(500);
+    }
+  }, 10000);
+
+server.listen(5000, () => console.log(`Running on the door 5000`))
