f5abd7f4d706d29a15a010597e047c04dca9228c
Antonio-Jefferson <antjeffersonbatista@gmail.com>
2023-01-14 19:29:38 -0300

    corrigindo erro de mensagem de entrada na sala

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { MongoClient } from "mongodb";
 import joi from "joi";
-import day from "dayjs";
+import dayjs from "dayjs";
 
 dotenv.config();
 const server = express();
@@ -30,19 +30,18 @@ const validationMessages = joi.object({
 })
 
 server.post("/participants", async (req, res) => {
-    const { name } = req.body;
-    const validation = validationName.validate(req.body);
-
-    if (validation.error) {
-        const error = validation.error.details[0].message;
-        res.status(422).send(error);
-        return;
-    }
-
     try {
+        const { name } = req.body;
+        const validation = validationName.validate(req.body);
+    
+        if (validation.error) {
+            const error = validation.error.details[0].message;
+            res.sendStatus(422);
+            return;
+        }
         const isValid = await db.collection("participants").findOne({ name: name });
         if (isValid) {
-            res.status(409).send("Usuario já existente!")
+            res.sendStatus(409)
             return;
         }
         const user =
@@ -53,11 +52,11 @@ server.post("/participants", async (req, res) => {
         await db.collection("participants").insertOne(user);
 
         const message = {
-            from: user,
+            from: name,
             to: "Todos",
             text: "entra na sala...",
             type: "status",
-            time: day().format("HH:mm:ss")
+            time: dayjs().format('HH:mm:ss')
         }
         await db.collection("messages").insertOne(message)
         res.sendStatus(201)
@@ -84,28 +83,31 @@ server.post("/messages", async (req, res) => {
     const { user } = req.headers;
     try {
         const messageUser = {
+            from: user,
             to,
             text,
             type,
-            from: user,
-            time: day().format("HH:mm:ss")
+            time: dayjs().format("HH:mm:ss")
         }
+
         const validateMessage = validationMessages.validate(messageUser);
         if (validateMessage.error) {
             const errors = validateMessage.error.details.map(item => item.message);
             res.status(422).send(errors);
             return;
         }
+
         const isParticipant = await db.collection("participants").findOne({ name: user });
         if (!isParticipant) {
             res.status(422).send("Usuario não existe");
             return;
         }
+
         await db.collection("messages").insertOne(messageUser);
         res.sendStatus(201)
+
     } catch (error) {
         res.sendStatus(500)
-        console.log(error)
     }
 })
 server.get("/messages", async (req, res) => {
@@ -155,7 +157,7 @@ setInterval(async () => {
               to: "Todos",
               text: "sai da sala...",
               type: "status",
-              time: day().format("HH:mm:ss"),
+              time: dayjs().format("HH:mm:ss"),
             };
           }
         );
@@ -163,7 +165,7 @@ setInterval(async () => {
         await db.collection("participants").deleteMany({ lastStatus: { $lte: seconds } });
       }
     } catch (error) {
-      res.status(500);
+      console.log(error);
     }
   }, 10000);
 
